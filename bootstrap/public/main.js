27717bf5e7fe76aeaa638cd63cee5573dca866e3
fang <test@qq.com>
2016-12-12 10:24:30 +0800

    first commit

--- bootstrap/public/main.js
+++ bootstrap/public/main.js
@@ -1,9 +1,30 @@
 $(function () {
 
-  $('asd').on('click', function (e) {
-    //获取添加商品的信息
+  $(window).on('hashchange', function () {
+    $('.sidebar li').removeClass('active');
+    hashFn();
+  });
+  hashFn();
 
-    get('list')
-  })
+  $('.main').on('click', '.btn-del', function (e) {
+    console.log($(this).parents('tr'));
+  });
+
+  function hashFn() {
+    var hash = location.hash;
+    if(hash == '#plist' || hash == ''){
+      $('.nav-plist').addClass('active');
+      render('#plist-tmpl', {});
+    }else if(hash == "#addp"){
+      $('.nav-addp').addClass('active');
+      render('#addp-tmpl', {});
+    }
+  }
+
+  function render(tmplId, data){
+    var tmpl = $(tmplId).html();
+    var html = ejs.render(tmpl, data);
+    $('.main').html(html);
+  }
 
 });
